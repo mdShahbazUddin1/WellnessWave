afa8f21d979d0f946461cd03d862fc2d6d4f5c39
jack <jackayron5@gmail.com>
2023-08-29 13:30:04 +0530

    m1

---
+++ frontend/src/screens/GoalSettingScreen.jsx
@@ -0,0 +1,46 @@
+// src/screens/GoalSettingScreen.js
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+
+const GoalSettingScreen = ({ navigation }) => {
+  const [goal, setGoal] = useState("");
+
+  const handleSaveGoal = () => {
+    // You can add logic here to save the user's goal
+    navigation.goBack(); // Navigate back to the previous screen
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Goal Setting</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your goal"
+        value={goal}
+        onChangeText={setGoal}
+      />
+      <Button title="Save Goal" onPress={handleSaveGoal} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+});
+
+export default GoalSettingScreen;
